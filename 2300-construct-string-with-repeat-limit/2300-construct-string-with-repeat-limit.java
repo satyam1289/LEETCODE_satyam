a87a6e3b2cd166dd5b47a8d32775c58377e75a14
Satyamsingh128 <114847183+satyam1289@users.noreply.github.com>
2024-12-17 20:43:29 +0530

    Time: 17 ms (92.8%), Space: 45.7 MB (50.42%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
@@ -4,14 +4,14 @@ class Solution {
         int n=s.length();
         StringBuilder sb=new StringBuilder();
 
-        // Step 1: Frequency count for each character
+       
         int[] arr=new int[26];
         for(int i=0; i<n; i++){
             arr[s.charAt(i)-'a']++;
         }
 
-        // Step 2: Iterate from larger characters to smaller characters
-        for(int i=25; i>=0; i--){ // i: One pointer
+        
+        for(int i=25; i>=0; i--){ 
             if(arr[i]==0) continue;
             
             int curr=Math.min(arr[i], repeatLimit);
@@ -21,11 +21,11 @@ class Solution {
                 curr--;
             }                        
 
-            // Step 3: Handle remaining occurrences by inserting smaller character
+          
             if(arr[i]!=0){
-                for(int j=i-1; j>=0; j--){ // j: Another pointer
+                for(int j=i-1; j>=0; j--){ 
                     if(arr[j]==0) continue;
-                    sb.append( (char)('a'+j) ); // Adding only one character                   
+                    sb.append( (char)('a'+j) );                  
                     arr[j]--;
                     i++;
                     break;
