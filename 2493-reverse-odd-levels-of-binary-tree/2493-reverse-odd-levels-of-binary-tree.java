edcafe9ad41a6899ebb177e7998454d5d884c5fc
Satyamsingh128 <114847183+satyam1289@users.noreply.github.com>
2024-12-20 16:34:45 +0530

    Time: 11 ms (24.54%), Space: 48.9 MB (35.24%) - LeetHub

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) return null;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        List<List<TreeNode>> levelNodesList = new ArrayList<>();
+
+        // Traverse the tree level by level
+        while (!nodeQueue.isEmpty()) {
+            int currentLevelSize = nodeQueue.size();
+            List<TreeNode> currentLevelNodes = new ArrayList<>();
+
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode currentNode = nodeQueue.poll();
+                currentLevelNodes.add(currentNode);
+                if (currentNode.left != null) {
+                    nodeQueue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    nodeQueue.add(currentNode.right);
+                }
+            }
+            levelNodesList.add(currentLevelNodes);
+        }
+
+        // Reverse values at odd levels
+        for (int levelIndex = 0; levelIndex < levelNodesList.size(); levelIndex++) {
+            if (levelIndex % 2 == 1) {
+                List<TreeNode> currentLevelNodes = levelNodesList.get(levelIndex);
+                int leftIndex = 0;
+                int rightIndex = currentLevelNodes.size() - 1;
+
+                while (leftIndex < rightIndex) {
+                    // Swap values
+                    int temp = currentLevelNodes.get(leftIndex).val;
+                    currentLevelNodes.get(leftIndex).val = currentLevelNodes.get(rightIndex).val;
+                    currentLevelNodes.get(rightIndex).val = temp;
+
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+        }
+
+        return root;
+    }
+}
